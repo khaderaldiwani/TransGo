b4296bb264d04edb01e508b669f2c3c0fbaa0854
laithdaher6 <daherlaith6@gmail.com>
2026-04-16 15:45:26 +0300

    remove the seeders

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -23,48 +23,48 @@ class AdminUserSeeder extends Seeder
                 'registration_type' => User::REGISTRATION_ADMIN,
             ]
         );
-        $user = User::updateOrCreate(
-            ['phone' => '0900009999'],
+        // $user = User::updateOrCreate(
+        //     ['phone' => '0900009999'],
             
-            [
-                'full_name' => 'Laith',
-                'email' => 'laith@example.com',
-                'password' => Hash::make('12345678'),
-                'must_change_password' => false,
-                'account_status' => User::STATUS_ACTIVE,
-                'registration_type' => User::REGISTRATION_ADMIN,
-            ]
-        );
-                $driver = User::updateOrCreate(
-            ['phone' => '0905609999'],
+        //     [
+        //         'full_name' => 'Laith',
+        //         'email' => 'laith@example.com',
+        //         'password' => Hash::make('12345678'),
+        //         'must_change_password' => false,
+        //         'account_status' => User::STATUS_ACTIVE,
+        //         'registration_type' => User::REGISTRATION_ADMIN,
+        //     ]
+        // );
+        //         $driver = User::updateOrCreate(
+        //     ['phone' => '0905609999'],
             
-            [
-                'full_name' => 'Laithdriver',
-                'email' => 'laithdriver@example.com',
-                'password' => Hash::make('12345678'),
-                'must_change_password' => false,
-                'account_status' => User::STATUS_ACTIVE,
-                'registration_type' => User::REGISTRATION_ADMIN,
-            ]
-        );
+        //     [
+        //         'full_name' => 'Laithdriver',
+        //         'email' => 'laithdriver@example.com',
+        //         'password' => Hash::make('12345678'),
+        //         'must_change_password' => false,
+        //         'account_status' => User::STATUS_ACTIVE,
+        //         'registration_type' => User::REGISTRATION_ADMIN,
+        //     ]
+        // );
 
         $adminRole = Role::where('name', Role::ROLE_ADMIN)->first();
-        $userRole = Role::where('name', Role::ROLE_PASSENGER)->first();
-        $driverRole = Role::where('name', Role::ROLE_DRIVER)->first();
+        // $userRole = Role::where('name', Role::ROLE_PASSENGER)->first();
+        // $driverRole = Role::where('name', Role::ROLE_DRIVER)->first();
 
         if (!$adminRole) {
             $adminRole = Role::create(['name' => Role::ROLE_ADMIN]);
         }
 
-        if (!$userRole) {
-            $userRole = Role::create(['name' => Role::ROLE_PASSENGER]);
-        }
-        if (!$driverRole) {
-            $driverRole = Role::create(['name' => Role::ROLE_DRIVER]);
-        }
+        // if (!$userRole) {
+        //     $userRole = Role::create(['name' => Role::ROLE_PASSENGER]);
+        // }
+        // if (!$driverRole) {
+        //     $driverRole = Role::create(['name' => Role::ROLE_DRIVER]);
+        // }
 
         $admin->roles()->syncWithoutDetaching([$adminRole->id]);
-        $user->roles()->syncWithoutDetaching([$userRole->id]);
-        $driver->roles()->syncWithoutDetaching([$driverRole->id]);
+        // $user->roles()->syncWithoutDetaching([$userRole->id]);
+        // $driver->roles()->syncWithoutDetaching([$driverRole->id]);
     }
 }
