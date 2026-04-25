27ff1b0581db71e6251ae24899ed50b2ef49a761
khaderaldiwani <khader.diwani@gmail.com>
2026-04-25 14:46:01 +0300

    gix problim

--- database/migrations/2026_04_17_120100_migrate_driver_receipts_to_receipts_table.php
+++ database/migrations/2026_04_17_120100_migrate_driver_receipts_to_receipts_table.php
@@ -36,11 +36,7 @@ return new class extends Migration
 
     private function dropWalletReceiptForeignIfExists(): void
     {
-<<<<<<< HEAD
         if (DB::getDriverName() !== 'mysql') {
-=======
-        if (DB::getDriverName() === 'sqlite') {
->>>>>>> 4b04ec6c9863e4a1e1fc4ef72ccb5cb1e7dc0070
             return;
         }
 
